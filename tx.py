e09958a55cf673838328043654d3d99592d92285
harikuts <harikuttivelil@gmail.com>
2022-01-16 09:46:46 -0800

    TX can now read from files and send over the network to RX which prints, but not yet stores.

--- tx.py
+++ tx.py
@@ -12,6 +12,11 @@ PORT = 1245
 import time
 import socket
 import random
+import os
+
+CUR_DIR = os.getcwd()
+OUTBOX_PATH = os.path.join(CUR_DIR, "outbox")
+MESSAGE_PATH = os.path.join(OUTBOX_PATH, "message.txt")
 
 # Get hostnames
 with open(NODELIST_FN, 'r') as f:
@@ -21,11 +26,18 @@ with open(NODELIST_FN, 'r') as f:
 while True:
     for host in nodelist:
         time.sleep(random.randint(0, 5))
+        # Get message from outbox.
+        try:
+            with open(MESSAGE_PATH, 'r') as f:
+                message = f.read()
+        except:
+            continue
+        # Open socket for sending message.
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             s.connect((host, PORT))
-            s.send(f"Hello! - {socket.gethostbyname(socket.gethostname())}".encode("utf-8"))
-            s.close()
+            s.send(message.encode("utf-8"))
         except Exception as e:
             pass
             print(f"Failed to connect to {host}. Skipping.")
+        s.close()
