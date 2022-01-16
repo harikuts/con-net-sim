35e84fcfacbb7ae1cb62af23dabb3c899efa27d9
harikuts <harikuttivelil@gmail.com>
2022-01-15 23:41:37 -0800

    Added base dummy training component. Instead of storing a model it just stores a message with the current timestamp.

--- Dockerfile
+++ Dockerfile
@@ -20,4 +20,4 @@ RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /
 USER appuser
 
 # During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-CMD python init.py ; python rx.py & python tx.py
+CMD python init.py ; python train.py & python rx.py & python tx.py
---
+++ train.py
@@ -0,0 +1,18 @@
+# Basic version should just publish time stamp to the outbox.
+import os
+import time
+import random
+import datetime
+
+def main():
+    while True:
+        time.sleep(random.randint(5, 7))
+        # Publish message
+        print("Publishing message.")
+        message = f"Hello. -- {datetime.datetime.now()}\n"
+        message_path = os.path.join(os.getcwd(), "outbox", "message.txt")
+        with open(message_path, 'w') as f:
+            f.write(message)
+
+if __name__ == "__main__":
+    main()
