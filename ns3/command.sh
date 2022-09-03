a0f660815207d288f5af7f6fc44736938c130027
Venkat Bollapragada <58750962+vbKenobi@users.noreply.github.com>
2022-09-03 13:51:02 -0700

    Changed command.sh to use relative paths

--- ns3/command.sh
+++ ns3/command.sh
@@ -1,1 +1,5 @@
-docker run -itd -w /ns-3 -v /Users/harikuts/playground/ns-3/ns-allinone-3.35/ns-3.35:/ns-3 ns3 bash -c "cd ns-3 && ./waf configure --enable-tests --enable-examples --out=build/debian"
+# docker run -itd -w /Users/venky/ns-3 -v /playground/ns-3/ns-allinone-3.35/ns-3.35:/ns-3 ns3 bash -c "cd ns-3 && ./waf configure --enable-tests --enable-examples --out=build/debian"
+test="/playground/ns-3/ns-allinone-3.35/ns-3.35:/ns-3 ns3 bash -c"
+relDir=$(echo $PWD | cut -d/ -f1-3)
+test="${relDir}${test}"
+docker run -itd -w /ns-3 -v $test "cd ns-3 && ./waf configure --enable-tests --enable-examples --out=build/debian"
